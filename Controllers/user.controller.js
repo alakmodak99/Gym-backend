ac732c01f02ec29dfeeb6747b2a8e132fef5bc60
unknown <alakmodak99@gmail.com>
2022-09-04 11:34:05 +0530

    m

--- Controllers/user.controller.js
+++ Controllers/user.controller.js
@@ -4,11 +4,12 @@ const bcryptjs = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const userModel = require("../Model/User.model")
 
-route.get("", async (req,res)=>{
+route.get("/", async (req,res)=>{
+    console.log("Alak")
    return res.status(200).send(await userModel.find())
 })
 
-route.post("register", async (req,res)=>{
+route.post("/register", async (req,res)=>{
     const check = await userModel.findOne({email: req.body.email})
     if(check) return res.status(400).send("Someone is already registerd with this email")
   let Salt = await bcryptjs.genSalt(10)
@@ -19,14 +20,14 @@ route.post("register", async (req,res)=>{
 
 // route.post("/verify-user",async(req,res)=>{
 //     try{
-//       var decoded = jwt.verify(req.params.id, 'alakmodak99');
+//       var decoded = jwt.verify(req.params.id, 'alakmodak');
 //       return res.status(200).send(decoded.email)
 //     }catch(err){
 //       return res.status(400).send(err);
 //     }
 //   })
 
-route.post("login", async (req,res)=>{
+route.post("/login", async (req,res)=>{
     const body = req.body
     const check = await userModel.findOne({email: body.email})
     if(check){
@@ -34,7 +35,7 @@ route.post("login", async (req,res)=>{
         const checkPass = await bcryptjs.compare(body.password, check.password)
         console.log(checkPass)
         if(checkPass){
-            const token = jwt.sign({ email: check.email }, 'alakmodak99');
+            const token = jwt.sign({ email: check.email }, 'alakmodak');
             return res.status(200).json({"message":"Login SuccessFull",token,user:check})
         }else{
             return res.status(400).json({"message":"Invalid Password"})
